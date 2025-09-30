75fbdccf37bca5e6e658629f2c1342948e0f363a
BenjaminAarhus <benjaminaarhus@yahoo.co.uk>
2025-09-30 13:46:30 +0100

    Read contents of Ouzel Dataset CSV

--- SDM.R
+++ SDM.R
@@ -2,3 +2,6 @@
 install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org")
 library(geodata)
 library(terra)
+
+# Read the contents of the csv file into a dataset
+ouzelData <- read.table('data/Data_SwissBreedingBirds.csv', header = T, sep = ',')
