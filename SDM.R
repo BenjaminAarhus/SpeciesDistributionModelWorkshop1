ebee874ccc90a610dea0f461358227d533925321
BenjaminAarhus <benjaminaarhus@yahoo.co.uk>
2025-09-30 13:56:57 +0100

    Create a smaller dataframe with fewer columns to remove excess data

--- SDM.R
+++ SDM.R
@@ -3,5 +3,13 @@ install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org"
 library(geodata)
 library(terra)
 
-# Read the contents of the csv file into a dataset
+# Read the contents of the csv file into a data set
 ouzelData <- read.table('data/Data_SwissBreedingBirds.csv', header = T, sep = ',')
+
+# Subset the data to remove excess data and leave only the relevant columns
+ouzelColumns <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
+ouzelDateFrame <-data.frame(ouzelData)[ouzelColumns]
+# Output result
+summary(ouzelDateFrame)
+
+
